035f4e238a8e9c50e6a34d9dbf4426e779eaed33
Максим Лебедев <bymsx@speckod.loc>
2017-05-05 21:09:11 +0300

    Исправление добавления товара в корзину и отображения корзины.

--- catalog/view/javascript/index.js
+++ catalog/view/javascript/index.js
@@ -18,25 +18,25 @@ opencartConfiguratorApp.controller('mainController', function ($scope, $location
         if (nextId >= 0) {
             $location.path('/stepProducts/' + nextId);
         } else if (nextId == -1) {
-            var selected = selectionService.getSelectedProducts();
-            var lastPromise;
-            angular.forEach(selected, function (val) {
-                var tmpPromise;
-
-                if (lastPromise) {
-                    lastPromise.then(function () {
-                        tmpPromise = nativeOpenCartService.addToCart(val);
-                    });
-
-                    lastPromise = tmpPromise;
-                } else {
-                    lastPromise = nativeOpenCartService.addToCart(val);
-                }
-            });
-
-            lastPromise.then(function () {
-                window.location.href = '/index.php?route=checkout/cart';
-            });
+            //var selected = selectionService.getSelectedProducts();
+            //var lastPromise;
+            //angular.forEach(selected, function (val) {
+            //    var tmpPromise;
+            //
+            //    if (lastPromise) {
+            //        lastPromise.then(function () {
+            //            tmpPromise = nativeOpenCartService.addToCart(val);
+            //        });
+            //
+            //        lastPromise = tmpPromise;
+            //    } else {
+            //        lastPromise = nativeOpenCartService.addToCart(val);
+            //    }
+            //});
+            //
+            //lastPromise.then(function () {
+            window.location.href = '/index.php?route=checkout/cart';
+            //});
 
         } else if (nextId == -2) {
             // не выбрано ни одной категории
@@ -44,19 +44,19 @@ opencartConfiguratorApp.controller('mainController', function ($scope, $location
     }
 
     $scope.next = function () {
-        //var lastProd = selectionService.getLastSelectedProduct();
-        //if (lastProd) {
-        //    nativeOpenCartService.addToCart(lastProd).then(function (isAdded) {
-        //        console.dir(isAdded);
-        //        if (isAdded) {
-        //            nextTab();
-        //        } else {
-        //            alert("Ошибка добавления товара в корзину.");
-        //        }
-        //    });
-        //} else {
-        //    nextTab();
-        //}
-        nextTab();
+        var lastProd = selectionService.getLastSelectedProduct();
+        if (lastProd) {
+            nativeOpenCartService.addToCart(lastProd).then(function (isAdded) {
+                console.dir(isAdded);
+                if (isAdded) {
+                    nextTab();
+                } else {
+                    alert("Ошибка добавления товара в корзину.");
+                }
+            });
+        } else {
+            nextTab();
+        }
+        //nextTab();
     };
 });
